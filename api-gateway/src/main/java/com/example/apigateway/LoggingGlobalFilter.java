ff704e48fa35b3d878b48a92e5768144388f1d9f
muratbek2255 <muratbek2255@gmail.com>
2023-04-24 15:32:18 +0600

    add api-gateway

---
+++ api-gateway/src/main/java/com/example/apigateway/LoggingGlobalFilter.java
@@ -0,0 +1,32 @@
+package com.example.apigateway;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import reactor.core.publisher.Mono;
+
+import java.util.Map;
+
+
+@Component
+public class LoggingGlobalFilter implements GlobalFilter {
+
+    final Logger logger = LoggerFactory.getLogger(LoggingGlobalFilter.class);
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
+        Map<String, String> headers = request.getHeaders().toSingleValueMap();
+
+        logger.info("REQUEST ID: " + request.getId());
+        logger.info("METHOD: " + request.getMethod().name());
+        logger.info("PATH: " + request.getPath());
+
+        return chain.filter(exchange);
+    }
+}
